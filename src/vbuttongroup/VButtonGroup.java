435444cc70afffe55bc473dac092d9245eae0a71
hubepa1516daw2 <hubepa1516daw2@gmail.com>
2016-05-07 22:29:57 +0200

    V0.1 MS-Servidor

---
+++ src/vbuttongroup/VButtonGroup.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vbuttongroup;
+
+/**
+     * 1) crear botones
+     * 2) arratrar group aunque momlo vemos
+     * 3)Vamso a  la pantalla navigator
+     * 3.1)Cambimos nombre variable 
+     * 4)añadimos en constructor los RB al GRB
+     * 5)Creamos evento:
+     * if(RB1.isSelected()) this.LBRB.setText(RB1.getText());
+     */
+public class VButtonGroup {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        V v = new V();
+        
+        v.setVisible(true);
+    }
+    
+}
